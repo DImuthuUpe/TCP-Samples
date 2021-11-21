a663d84e0057372dc0b454bf8dedbaffed931c7a
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2021-11-20 20:07:50 -0500

    Minor change

--- udp-client.c
+++ udp-client.c
@@ -31,7 +31,7 @@ int main() {
 	// Filling server information
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(PORT);
-	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	
 	int n, len;
 	int i;
